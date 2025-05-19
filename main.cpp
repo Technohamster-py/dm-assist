4c96543b27b463c15451f1f8c5f8183e9180b92f
aakhomchenko <homchenko_aa@nrcki.ru>
2025-05-19 13:02:30 +0300

    Добавлен загрузочный экран

--- main.cpp
+++ main.cpp
@@ -1,16 +1,18 @@
 #include <QApplication>
-
+#include <QPixmap>
+#include <QSplashScreen>
 #include "mainwindow.h"
-
-
-#include <QDebug>
 
 
 int main(int argc, char *argv[]) {
     QApplication a(argc, argv);
-
+    QPixmap pixmap(":/splash/splash.png");
+    QSplashScreen splashScreen(pixmap);
+    splashScreen.show();
+    splashScreen.showMessage("Loading application", Qt::AlignBottom | Qt::AlignCenter, Qt::white);
     MainWindow w;
     w.show();
 
+    splashScreen.finish(&w);
     return a.exec();
 }
