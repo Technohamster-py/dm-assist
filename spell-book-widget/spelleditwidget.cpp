f158a18422284c3d28aaffe0e127048885aa1a94
Technohamster <arseniy.khomchenko@gmail.com>
2025-12-19 13:43:17 +0300

    Реализован парсер

--- spell-book-widget/spelleditwidget.cpp
+++ spell-book-widget/spelleditwidget.cpp
@@ -1,14 +1,47 @@
-// You may need to build the project (run Qt uic code generator) to get "ui_SpellEditWidget.h" resolved
-
 #include "spelleditwidget.h"
 #include "ui_spelleditwidget.h"
 
+#include <QJsonObject>
+#include "../map-widget/texturepickerdialog.h"
 
 SpellEditWidget::SpellEditWidget(QWidget *parent) :
         QWidget(parent), ui(new Ui::SpellEditWidget) {
     ui->setupUi(this);
+
+    connect(ui->materialCheckBox, &QCheckBox::toggled, ui->materialList, &QTextEdit::setEnabled);
+    connect(ui->textureButton, &QPushButton::clicked, [=](){
+        QString textureName = TexturePickerDialog::getTexture(this);
+        ui->textureButton->setIcon(QIcon(textureName));
+    });
 }
 
 SpellEditWidget::~SpellEditWidget() {
     delete ui;
+}
+
+bool SpellEditWidget::parseFromJson(QJsonObject json) {
+    ui->titleEdit->setText(json.value("name").toString());
+    ui->levelBox->setValue(json.value("level").toInt());
+    ui->descriptionEdit->setCustomHtml(json["description"].toObject().value("value").toString());
+
+    ui->verbalCheckBox->setChecked(json["components"].toObject().value("vocal").toBool());
+    ui->somaticCheckBox->setChecked(json["components"].toObject().value("somatic").toBool());
+    ui->materialCheckBox->setChecked(json["components"].toObject().value("material").toBool());
+    if (ui->materialCheckBox->isChecked())
+        ui->materialList->setText(json["materials"].toObject().value("value").toString());
+
+    ui->castingUnitsBox->setCurrentIndex(m_casting[json["activation"].toObject().value("type").toString()]);
+    ui->castingTimeBox->setValue(json["activation"].toObject().value("cost").toInt());
+
+    ui->durationBox->setValue(json["duration"].toObject().value("value").toInt());
+    ui->durationUnitsBox->setCurrentIndex(m_duration[json["duration"].toObject().value("units").toString()]);
+
+    ui->rangeBox->setValue(json["range"].toObject().value("value").toInt());
+    ui->rangeUnitsBox->setCurrentIndex(m_units[json["range"].toObject().value("units").toString()]);
+
+    ui->aoeBox->setValue(json["target"].toObject().value("value").toInt());
+    ui->aoeUnitsBox->setCurrentIndex(m_units[json["target"].toObject().value("units").toString()]);
+    ui->shapeComboBox->setCurrentIndex(m_shapes[json["target"].toObject().value("type").toString()]);
+
+    return true;
 }
--- spell-book-widget/spelleditwidget.h
+++ spell-book-widget/spelleditwidget.h
@@ -16,8 +16,41 @@ public:
 
     ~SpellEditWidget() override;
 
+protected:
+    bool parseFromJson(QJsonObject json);
 private:
     Ui::SpellEditWidget *ui;
+
+    QMap<QString, int> m_casting{
+            {"action", 0},
+            {"reaction", 1},
+            {"bonus", 2},
+            {"minute", 3},
+            {"hour", 4}
+    };
+
+    QMap<QString, int> m_duration{
+            {"round", 0},
+            {"minute", 1},
+            {"hour", 2},
+            {"day", 3},
+            {"year", 4},
+            {"perm", 5}
+    };
+
+    QMap<QString, int> m_units{
+            {"ft", 0},
+            {"miles", 1}
+    };
+
+    QMap<QString, int> m_shapes{
+            {"line", 0},
+            {"cone", 1},
+            {"sphere", 2},
+            {"cube", 3},
+            {"cylinder", 4},
+            {"wall", 0}
+    };
 };
 
 
